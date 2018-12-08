1a3ca61ef5357082011ce8319a8ac9c6ff279abf
Célian Garcia <celian.garcia1@gmail.com>
2018-12-08 13:37:45 +0100

    Updated the data model with a new intent ADD_TO_LIST, will miss the recognition of entities

--- Readme.md
+++ Readme.md
@@ -1,9 +1,19 @@
 # Installation
 ```bash
 python -m spacy download fr
+python -m spacy download fr_core_news_md
 pip install -r requirements.txt
 pip install -e .
 ```
+
+# Update the learning sentences
+Update the spacy_sandbox/simple_cats.py file with the new sentences and categories.
+
+```bash
+python spacy_sandbox/simple_cats.py
+```
+
+It might take a few minutes.
 
 # Run
 ```bash
--- spacy_sandbox/simple_cats.py
+++ spacy_sandbox/simple_cats.py
@@ -1,5 +1,4 @@
 import spacy
-nlp = spacy.load('fr')
 
 train_data = [
     (u"quelle heure il est", {"cats": {"GET_TIME": 1, "HELLO": 0, "LAST_SHOWTIME": 0}}),
@@ -15,14 +14,28 @@ train_data = [
     (u"il y a quoi au cinéma", {"cats": {"GET_TIME": 0, "HELLO": 0, "LAST_SHOWTIME": 1}}),
     (u"qu'est ce qu'il y a au ciné ce soir", {"cats": {"GET_TIME": 0, "HELLO": 0, "LAST_SHOWTIME": 1}}),
     (u"programme ciné", {"cats": {"GET_TIME": 0, "HELLO": 0, "LAST_SHOWTIME": 1}}),
-    (u"j'aimerais connaître le programme du cinéma", {"cats": {"GET_TIME": 0, "HELLO": 0, "LAST_SHOWTIME": 1}}),
+
+    (u"ajoute des pâtes à ma liste", {"cats": {"ADD_TO_LIST": 1}}),
+    (u"ajoute des tomates à ma liste de courses", {"cats": {"ADD_TO_LIST": 1}}),
+
 ]
 
+
+all_categories = set([])
+for doc, gold in train_data:
+    for cat_key, cat_val in gold['cats'].items():
+        all_categories.add(cat_key)
+for doc, gold in train_data:
+    for cat in all_categories:
+        if cat not in gold['cats']:
+            gold['cats'][cat] = 0
+
+
+nlp = spacy.load('fr_core_news_md')
 textcat = nlp.create_pipe('textcat')
 nlp.add_pipe(textcat, last=True)
-textcat.add_label('GET_TIME')
-textcat.add_label('HELLO')
-textcat.add_label('LAST_SHOWTIME')
+for cat in all_categories:
+    textcat.add_label(cat)
 optimizer = nlp.begin_training()
 for itn in range(5):
     for doc, gold in train_data:
